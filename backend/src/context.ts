3e38029db82ce1883113dfcb978550b534818ba6
Mikhail Guskov <meguskov@gmail.com>
2019-10-28 22:45:34 +0300

    remove files + fixes

--- backend/src/context.ts
+++ backend/src/context.ts
@@ -1,15 +1,11 @@
 import { Photon } from '@generated/photon'
-import { ContextParameters } from 'graphql-yoga/dist/types'
+
 const photon = new Photon()
 
 export interface Context {
   photon: Photon
-  request: any
 }
 
-export function createContext(request: ContextParameters) {
-  return {
-    ...request,
-    photon,
-  }
+export function createContext(): Context {
+  return { photon }
 }
